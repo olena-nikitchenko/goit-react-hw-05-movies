1317a16049314bb0532d299ccd3c237a229cfdf3
olena-nikitchenko <helnikitka@gmail.com>
2023-05-22 21:17:00 +0300

    added button and change styled searchbar

--- src/components/Searchbar/Searchbar.styled.js
+++ src/components/Searchbar/Searchbar.styled.js
@@ -18,23 +18,40 @@ export const SearchForm = styled.form`
   max-width: 600px;
   background-color: #fff;
   border-radius: 3px;
+  border: 2px solid #274cdd;
   overflow: hidden;
 `;
 
+export const Button = styled.button`
+  display: inline-block;
+  width: 48px;
+  height: 48px;
+  border: 0;
+  background-size: 40%;
+  background-repeat: no-repeat;
+  background-position: center;
+  opacity: 0.6;
+  transition: opacity 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  cursor: pointer;
+  outline: none;
+`;
+
 export const Icon = styled(HiSearch)`
-  width: 20px;
-  height: 20px;
-  position: absolute;
-  right: 6px;
+  width: 30px;
+  height: 30px;
   fill: #274cdd;
 `;
 
 export const SearchFormInput = styled.input`
+  display: inline-block;
   width: 100%;
-  padding: 8px 32px 8px 8px;
-  border-radius: 2px;
   font: inherit;
-  border-color: #274cdd;
+  font-size: 20px;
+  border: none;
+  outline: none;
+  padding-left: 4px;
+  padding-right: 4px;
+  color: #274cdd;
 
   ::placeholder {
     font: inherit;
